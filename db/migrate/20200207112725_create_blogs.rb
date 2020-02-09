9e52b8d0e61fdd058f093887a0dc839ce1fcaadb
岡部大志 <taishi@bokunoMacBook-Pro.local>
2020-02-09 14:13:58 +0900

    [finish]4-2

--- db/migrate/20200207112725_create_blogs.rb
+++ db/migrate/20200207112725_create_blogs.rb
@@ -1,9 +1,10 @@
 class CreateBlogs < ActiveRecord::Migration[5.2]
   def change
     create_table :blogs do |t|
-    	t.title :string
-    	t.category :string
-    	t.body :text
+
+ 		t.string :title
+    	t.string :category
+    	t.text :blogs
 
 		t.timestamps
     end
